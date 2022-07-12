757708f21e278b7046bbca7eab98e2de596d8d7b
sinastya <sinastya@yandex.ru>
2022-07-13 00:11:57 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let new_keys = Object.keys(MORSE_TABLE).map(elem => '0'.repeat(10 - elem.length * 2) + `${elem.replace(/-/g, '11').replace(/\./g, '10')}`);
+    let values = [].concat(...Object.entries(MORSE_TABLE)).filter((elem, index) => (index % 2 == 1) ? elem: '')
+    let new_morse_table = {};
+    values.forEach((letter, index) => {
+    new_morse_table[new_keys[index]] = values[index]
+    })
+    new_morse_table['**********'] = " "
+
+    let decoded = expr.split('');
+    for (let i=10; i < expr.length * 1.1; i+= 11) {
+        decoded.splice(i, 0, ' ')
+    }
+    return decoded.join('').trim().split(' ').map(elem => new_morse_table[elem]).join('')
 }
 
 module.exports = {
